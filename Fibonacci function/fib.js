4b834633a5a0657a0e3347b1bc653eec24dc2ab2
Amber Wilson <you@example.com>
2023-03-11 07:59:35 -0600

    set up first Fibonacci problem

--- Fibonacci function/fib.js
+++ Fibonacci function/fib.js
@@ -6,6 +6,13 @@ const fib = (n) => {
     // But in the recursive case in general (baked in), return the sum 
     // of the Fibonacci number right before the one I'm asking for, 
     // as well as the Fibonacci number two before the one I'm asking for.
-    return fib(n - 1) + fib(n-2)
+    return fib(n - 1) + fib(n-2);
+};
 
-}
+// Test
+console.log(fib(6));
+console.log(fib(7));
+console.log(fib(8));
+// This number is too big and keeps running and is too slow so back to the drawing board
+// DRAW IT OUT
+console.log(fib(50));
